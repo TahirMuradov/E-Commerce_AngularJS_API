9470a44c640f496d860c34dae39568a2d8aa6e97
TahirMuradov <muradovtahir01@gmail.com>
2025-07-22 11:34:52 +0400

    AzureStorageServiceAdded

---
+++ Shop.Infrastructure/Services/Storages/Azure/AzureStorage.cs
@@ -0,0 +1,55 @@
+﻿using Shop.Application.Abstraction.Storage.Azure;
+using Shop.Application.Abstraction.Storage;
+using Microsoft.AspNetCore.Http;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Shop.Infrastructure.Utilities;
+
+namespace Shop.Infrastructure.Services.Storages.Azure
+{
+  public  class AzureStorage: Storage, IAzureStorage
+    {
+        readonly BlobServiceClient _blobServiceClient;
+        BlobContainerClient _blobContainerClient;
+        public AzureStorage()
+        {
+            _blobServiceClient = new(ConfigurationInfrastructure.config["Storage:Azure"]);
+        }
+        public async Task DeleteAsync(string containerName, string fileName)
+        {
+            _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            BlobClient blobClient = _blobContainerClient.GetBlobClient(fileName);
+            await blobClient.DeleteAsync();
+        }
+
+        public List<string> GetFiles(string containerName)
+        {
+            _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            return _blobContainerClient.GetBlobs().Select(b => b.Name).ToList();
+        }
+
+        public bool HasFile(string containerName, string fileName)
+        {
+            _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            return _blobContainerClient.GetBlobs().Any(b => b.Name == fileName);
+        }
+
+        public async Task<List<(string fileName, string pathOrContainerName)>> UploadAsync(string containerName, IFormFileCollection files)
+        {
+            _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            await _blobContainerClient.CreateIfNotExistsAsync();
+            await _blobContainerClient.SetAccessPolicyAsync(PublicAccessType.BlobContainer);
+
+            List<(string fileName, string pathOrContainerName)> datas = new();
+            foreach (IFormFile file in files)
+            {
+                string fileNewName = await FileRenameAsync(containerName, file.Name, HasFile);
+
+                BlobClient blobClient = _blobContainerClient.GetBlobClient(fileNewName);
+                await blobClient.UploadAsync(file.OpenReadStream());
+                datas.Add((fileNewName, $"{containerName}/{fileNewName}"));
+            }
+            return datas;
+        }
+    }
+}
